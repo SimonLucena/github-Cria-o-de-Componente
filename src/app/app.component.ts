6b7568ed83a5866afe863ded325fbafff001c018
SimonLucena <112531561+SimonLucena@users.noreply.github.com>
2022-10-05 11:28:16 -0300

    3 files modified, 4 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import {Component} from '@angular/core';
-import {Usuario} from '../shared/model/usuario';
+import { Component } from '@angular/core';
+import { Usuario } from '../shared/model/usuario';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   titulo = 'Social IFPB';
@@ -20,5 +20,4 @@ export class AppComponent {
     this.usuarios.push(this.usuario);
     this.usuario = new Usuario();
   }
-
 }
---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MenuComponent } from './menu/menu.component';
+
+@NgModule({
+  declarations: [MenuComponent],
+  imports: [
+    BrowserAnimationsModule,
+    CommonModule,
+    MatButtonModule,
+    MatMenuModule,
+  ],
+  exports: [MenuComponent],
+})
+export class LayoutModule {}
